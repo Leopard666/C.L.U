4f118af577aa409149f486b6b3b5d3f15be53a28
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-09 05:16:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,18 +41,18 @@ client.on('message', message => {
   client.on('ready', function(){
   client.user.setStatus("dnd");
     var ms = 10000 ;
-    var setGame = ['★ TG | System ★','The Grid™ | Server ' ];
+    var setActivity = ['★ TG | System ★','The Grid™ | Server ' ];
     var i = -1;
     var j = 0;
     setInterval(function (){
         if( i == -1 ){
             j = 1;
         }
-        if( i == (setGame.length)-1 ){
+        if( i == (setActivity.length)-1 ){
             j = -1;
         }
         i = i+j;
-        client.user.setGame(setGame[i],`https://www.twitch.tv/TheRealPredvkill`);
+        client.user.setActivity(setActivity[i],`https://www.twitch.tv/TheRealPredvkill`);
     }, ms);
 });
 
