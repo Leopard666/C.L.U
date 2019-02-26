90a408b4d7489aa37742701b18c4a96d890a0a8e
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-26 19:16:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,8 +42,7 @@ function timeCon(time) {
 	
 var version = '1.9';
 client.on('ready', function(){
-client.channels.get("542905235241304065").send("**🚀 [C.L.U] IS BACK ONLINE NOW 🚀**")
-	     const embed = new Discord.RichEmbed()
+client.channels.get("542905235241304065").send("🚀 [C.L.U] IS BACK ONLINE NOW 🚀
             .setAuthor(client.user.username,client.user.avatarURL)
             .setThumbnail(client.user.avatarURL)
             .setColor('RANDOM')
@@ -58,7 +57,7 @@ client.channels.get("542905235241304065").send("**🚀 [C.L.U] IS BACK ONLINE NO
             .addField('``Node :``' , `[${process.version} ]` , true)
                   .addField('``Bot Language :``' , `[ Java Script ]` , true)
 	          .addField('**Bot Owner** :' , `[<@480540559233122324>]` , true)
-                  .setFooter('🔰 THE GRID™ - OFFICIAL 🔰')
+                  .setFooter('🔰 THE GRID™ - OFFICIAL 🔰')")
    
    });
 
