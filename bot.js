4915306120c6028431057c6319e0e331b208ac82
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-26 18:56:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,53 +26,15 @@ const prefix = '/'
 ti={}  
 spee={};
 
-// ==================================================================
+// ================================================================
 
-function timeCon(time) {
-    let days = Math.floor(time % 31536000 / 86400)
-    let hours = Math.floor(time % 31536000 % 86400 / 3600)
-    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
-    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
-    days = days > 9 ? days : '0' + days
-    hours = hours > 9 ? hours : '0' + hours
-    minutes = minutes > 9 ? minutes : '0' + minutes
-    seconds = seconds > 9 ? seconds : '0' + seconds
-    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
+let channel = client.channels.get('542905235241304065');
+if(channel) {
+    channel.send("Uptime  : [timeCon(process.uptime())]");
 }
-var version = '1.9';
+});
 
-client.channels.get("542905235241304065").send("**🚀 [C.L.U] IS BACK ONLINE NOW 🚀**")
-	     const embed = new Discord.RichEmbed()
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('RANDOM')
-            .setTitle('**🚀 [C.L.U] IS BACK ONLINE NOW 🚀**')
-            .addField('``Uptime``', [timeCon(process.uptime())], true)
-            .addField('``Bot RAM Usage :``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
-            .addField('``servers``', [client.guilds.size], true)
-            .addField('``Channels :``' , `[ ${client.channels.size} ]` , true)
-            .addField('``Users :``' ,`[ ${client.users.size} ]` , true)
-            .addField('``Bot Name :``' , `[ ${client.user.tag} ]` , true)
-            .addField('``Bot ID :``' , `[ ${client.user.id} ]` , true)
-            .addField('``Node :``' , `[${process.version} ]` , true)
-                  .addField('``Bot Language :``' , `[ Java Script ]` , true)
-	          .addField('**Bot Owner** :' , `[<@480540559233122324>]` , true)
-                  .setFooter('🔰 THE GRID™ - OFFICIAL 🔰')
-
-          message.channel.sendEmbed(embed);
-   
-   });
-	    
-// ==================================================================
-
-
-
-
-
-
-
-
-
+// ================================================================
 
 client.on('message', message => {
   if (message.author.bot) return;
