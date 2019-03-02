42d33a56599f9355893abaff2d6723c90b2e9249
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-03-02 23:39:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ client.on('message', message => {
 
 client.on("disconnect", data => {
 client.channels.get("542905235241304065").send("/C.L.U IS OFFLINE NOW").then(m => m.delete(500));
-}	    
+	    
 ));
 
 // ================================================================
